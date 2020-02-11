5677406050940c18ed03e6154ccb0a3ebb089ea1
”名前” <>
2020-02-11 12:20:57 +0000

    課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -28,3 +28,49 @@ public function add()
     }
 }
 
+//【応用】 Admin/ProfileControllerに、以下のadd, create, edit, update それぞれのActionを追加してみましょう。
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+   
+   //3 【応用】 前々章でAdmin/ProfileControllerのadd Action, edit Action に次のように記述しました。 
+      public function add()
+  {
+      return view('admin.profile.create');
+  }
+  public function edit()
+  {
+      return view('admin.profile.edit');
+  }
+  
+  
+ //【応用】 3. の答えを実際に作成してみましょう。また、作成したbladeファイルにhtmlで記述して装飾してみましょう。
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta http=equiv="X-UA-Compatible" "content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <title>課題</title>
+    </head>
+    <body>
+     <h1>課題10</h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 });
+
+
+//URLとControllerやActionを紐付ける機能を何といいますか？
+//Routing機能
+
+//あなたが考える、group化をすることのメリットを考えてみてください。
+//管理しやすくなる。
+
+
+//「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
+
+Route::get('admin/news/create', 'Admin\AAAController@bbb');
+
+//【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の
+//add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。
+
+Route::get('admin/profile/create','Admin\ProfileController@add');
+Route::get('admin/news/create', "admin/profile/edit" );
