0691041b5eb19c8649264b51beceaf0423972839
”名前” <>
2020-02-19 12:49:18 +0000

    2020年2月19日 21:49:11

--- app/News.php
+++ app/News.php
@@ -15,17 +15,3 @@ class News extends Model
     );
 }
 
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class database extends Model
-{
-    protected $guarded = array('id');
-
-    // 以下を追記
-    public static $rules = array(
-        'title' => 'required',
-        'body' => 'required',
-    );
-}
